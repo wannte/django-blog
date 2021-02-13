00c2d47b0b01c893ac19595990e3f4403a658e44
Wannte <T.wannte@gmail.com>
2021-02-13 22:33:07 +0900

    feat: post_list css 추가

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'djangoProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'articles/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
